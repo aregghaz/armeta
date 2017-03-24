3e70503edade6c67b5d226a90586943adb027083
Areg <aregghaz@gmail.com>
2017-03-24 14:39:04 +0400

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://armeta.am/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
