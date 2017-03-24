d9a2e064932196cf977d80eded99d082b07bb734
Areg <aregghaz@gmail.com>
2017-03-24 15:02:59 +0400

    Update header.php

--- application/views/header.php
+++ application/views/header.php
@@ -11,10 +11,10 @@
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
           integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo base_url() ?>css/form.css">
-    <link rel="stylesheet" href="<?php echo base_url() ?>css/owl.carousel.css">
-    <link rel="stylesheet" href="<?php echo base_url() ?>css/slider.css">
-    <link rel="stylesheet" href="<?php echo base_url() ?>css/style.css">
+    <link rel="stylesheet" href="<?php  base_url() ?>css/form.css">
+    <link rel="stylesheet" href="<?php  base_url() ?>css/owl.carousel.css">
+    <link rel="stylesheet" href="<?php base_url() ?>css/slider.css">
+    <link rel="stylesheet" href="<?php  base_url() ?>css/style.css">
     <script src="<?php base_url() ?>js/jquery.js"></script>
     <script src="<?php  base_url() ?>js/jquery-migrate-1.1.1.js"></script>
     <script src="<?php  base_url() ?>js/script.js"></script>
