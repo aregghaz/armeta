13ae0d4b07fd3ec82d072c65ab46ad60c0083fe1
Areg <aregghaz@gmail.com>
2017-03-24 15:12:23 +0400

    Update header.php

--- application/views/header.php
+++ application/views/header.php
@@ -11,22 +11,22 @@
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
           integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <link rel="stylesheet" href="armeta.am/css/form.css">
-    <link rel="stylesheet" href="armeta.am/css/owl.carousel.css">
-    <link rel="stylesheet" href="armeta.am/css/slider.css">
-    <link rel="stylesheet" href="armeta.am/css/style.css">
-    <script src="armeta.am/js/jquery.js"></script>
-    <script src="armeta.am/js/jquery-migrate-1.1.1.js"></script>
-    <script src="armeta.am/js/script.js"></script>
-    <script src="armeta.am/js/jquery.ui.totop.js"></script>
-    <script src="armeta.am/js/superfish.js"></script>
-    <script src="armeta.am/js/jquery.equalheights.js"></script>
-    <script src="armeta.am/js/jquery.mobilemenu.js"></script>
-    <script src="armeta.am/js/jquery.easing.1.3.js"></script>
-    <script src="armeta.am/js/owl.carousel.js"></script>
-    <script src="armeta.am/js/jquery.flexslider-min.js"></script>
-    <script src="armeta.am/js/kwiks.js"></script>
-    <script src="armeta.am/js/TMForm.js"></script>
+    <link rel="stylesheet" href="http://armeta.am/css/form.css">
+    <link rel="stylesheet" href="http://armeta.am/css/owl.carousel.css">
+    <link rel="stylesheet" href="http://armeta.am/css/slider.css">
+    <link rel="stylesheet" href="http://armeta.am/css/style.css">
+    <script src="http://armeta.am/js/jquery.js"></script>
+    <script src="http://armeta.am/js/jquery-migrate-1.1.1.js"></script>
+    <script src="http://armeta.am/js/script.js"></script>
+    <script src="http://armeta.am/js/jquery.ui.totop.js"></script>
+    <script src="http://armeta.am/js/superfish.js"></script>
+    <script src="http://armeta.am/js/jquery.equalheights.js"></script>
+    <script src="http://armeta.am/js/jquery.mobilemenu.js"></script>
+    <script src="http://armeta.am/js/jquery.easing.1.3.js"></script>
+    <script src="http://armeta.am/js/owl.carousel.js"></script>
+    <script src="http://armeta.am/js/jquery.flexslider-min.js"></script>
+    <script src="http://armeta.am/js/kwiks.js"></script>
+    <script src="http://armeta.am/js/TMForm.js"></script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
             integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
