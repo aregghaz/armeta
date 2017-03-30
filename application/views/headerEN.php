9576a37d9746604e2c1d878be395f5efd53877e4
Admin <mhertamazyan>
2017-03-30 12:25:59 +0400

    firs commit

--- application/views/headerEN.php
+++ application/views/headerEN.php
@@ -14,22 +14,22 @@
           href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-formhelpers/2.3.0/css/bootstrap-formhelpers.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
           integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo base_url() ?>/css/form.css">
-    <link rel="stylesheet" href="<?php echo base_url() ?>/css/owl.carousel.css">
-    <link rel="stylesheet" href="<?php echo base_url() ?>/css/slider.css">
-    <link rel="stylesheet" href="<?php echo base_url() ?>/css/style.css">
-    <script src="<?php echo base_url() ?>/js/jquery.js"></script>
-    <script src="<?php echo base_url() ?>/js/jquery-migrate-1.1.1.js"></script>
-    <script src="<?php echo base_url() ?>/js/script.js"></script>
-    <script src="<?php echo base_url() ?>/js/jquery.ui.totop.js"></script>
-    <script src="<?php echo base_url() ?>/js/superfish.js"></script>
-    <script src="<?php echo base_url() ?>/js/jquery.equalheights.js"></script>
-    <script src="<?php echo base_url() ?>/js/jquery.mobilemenu.js"></script>
-    <script src="<?php echo base_url() ?>/js/jquery.easing.1.3.js"></script>
-    <script src="<?php echo base_url() ?>/js/owl.carousel.js"></script>
-    <script src="<?php echo base_url() ?>/js/jquery.flexslider-min.js"></script>
-    <script src="<?php echo base_url() ?>/js/kwiks.js"></script>
-    <script src="<?php echo base_url() ?>/js/TMForm.js"></script>
+    <link rel="stylesheet" href="http://armeta.am/css/form.css">
+    <link rel="stylesheet" href="http://armeta.am/css/owl.carousel.css">
+    <link rel="stylesheet" href="http://armeta.am/css/slider.css">
+    <link rel="stylesheet" href="http://armeta.am/css/style.css">
+    <script src="http://armeta.am/js/jquery.js"></script>
+    <script src="http://armeta.am/js/jquery-migrate-1.1.1.js"></script>
+    <script src="http://armeta.am/js/script.js"></script>
+    <script src="http://armeta.am/js/jquery.ui.totop.js"></script>
+    <script src="http://armeta.am/js/superfish.js"></script>
+    <script src="http://armeta.am/js/jquery.equalheights.js"></script>
+    <script src="http://armeta.am/js/jquery.mobilemenu.js"></script>
+    <script src="http://armeta.am/js/jquery.easing.1.3.js"></script>
+    <script src="http://armeta.am/js/owl.carousel.js"></script>
+    <script src="http://armeta.am/js/jquery.flexslider-min.js"></script>
+    <script src="http://armeta.am/js/kwiks.js"></script>
+    <script src="http://armeta.am/js/TMForm.js"></script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
             integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
@@ -186,8 +186,8 @@
                         <li><a href="http://armeta.am/index.php/PagesEN/education">Education</a></li>
                         <li><a href="http://armeta.am/index.php/PagesEN/video">Videos</a></li>
                         <li><a href="http://armeta.am/index.php/PagesEN/partners">Partners</a></li>
-                        <li><a href="http://armeta.am>/index.php/PagesEN/news">News</a></li>
-                        <li><a href="http://armeta.am/PagesEN/contacts">Contact Us</a></li>
+                        <li><a href="http://armeta.am/index.php/PagesEN/news">News</a></li>
+                        <li><a href="http://armeta.am/index.php/PagesEN/contacts">Contact Us</a></li>
                     </ul>
                 </nav>
                 <div class="clear"></div>
